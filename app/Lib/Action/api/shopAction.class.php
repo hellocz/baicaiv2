2f54985c8c4b6a2fdf3f664549f9b9e6eaddd23f
hellocz <chenzhihn84@163.com>
2017-05-31 11:36:00 +0800

    remove the price label and add the  price and time

--- app/Lib/Action/api/shopAction.class.php
+++ app/Lib/Action/api/shopAction.class.php
@@ -179,7 +179,7 @@ class shopAction extends userbaseAction
         if($data['type'] == 1){
             $list=M()->query("SELECT id,title,img,price from try_item  WHERE add_time between $time_hour and $time ORDER BY hits desc,add_time desc LIMIT 9");
         }else{
-            $list=M()->M()->query("SELECT id,title,img,price from try_item  WHERE add_time between $time_day and $time ORDER BY hits desc,add_time desc LIMIT 9");
+            $list=M()->query("SELECT id,title,img,price from try_item  WHERE add_time between $time_day and $time ORDER BY hits desc,add_time desc LIMIT 9");
         }
 
         foreach ($list as $key => &$val) {
